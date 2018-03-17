3807c6ae4aa3d0da4acb7b316ccc88ba6d319f96
heinhtetaung-node <heinhtetaung.sglife@gmail.com>
2018-03-17 23:59:12 +0630

    https://www.bitovi.com/blog/introducing-react-view-model-mvvm-with-react
    https://www.bitovi.com/blog/introducing-react-view-model-mvvm-with-react

--- resources/assets/js/View/CreateItem.js
+++ resources/assets/js/View/CreateItem.js
@@ -1,39 +1,51 @@
 import React, {Component} from 'react';
 import {browserHistory} from 'react-router';
+import reactViewModel from 'react-view-model';
 import ItemViewModel from '../ViewModel/ItemViewModel';
+import BaseViewModel from '../ViewModel/BaseViewModel';
 
-var $this;
-class CreateItem extends ItemViewModel {
+const AppComponent = reactViewModel(ItemViewModel, (vm) => {
+    return (
+        
+        <div onClick={ () => vm.increment() }>
+            Count: {vm.count} (Click Me)
+        </div>
+  
+        // <div>
+        //     <h1>Create An Item</h1>
+        //     <form onSubmit={ () => vm.handleSubmit() }>
+        //         <div className="row">
+        //             <div className="col-md-6">
+        //                 <div className="form-group">
+        //                     <label>Item Name: {vm.productName} </label>
+        //                     <input type="text" className="form-control" onChange={vm.handleChange1} />
+        //                 </div>
+        //             </div>
+        //         </div>
+        //         <div className="row">
+        //             <div className="col-md-6">
+        //                 <div className="form-group">
+        //                     <label>Item Price:</label>
+        //                     <input type="text" className="form-control col-md-6" onChange={vm.handleChange2} />
+        //                 </div>
+        //             </div>
+        //         </div>
+        //         <br />
+        //         <div className="form-group">
+        //             <button className="btn btn-primary">Add Item</button>
+        //         </div>
+        //     </form>
+        // </div>
+    );
+});
+
+class CreateItem extends BaseViewModel {
     constructor(props){
         super(props);                
     }
     render() {
         return (
-            <div>
-                <h1>Create An Item</h1>
-                <form onSubmit={this.handleSubmit}>
-                    <div className="row">
-                        <div className="col-md-6">
-                            <div className="form-group">
-                                <label>Item Name:</label>
-                                <input type="text" className="form-control" onChange={this.handleChange1} />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-6">
-                            <div className="form-group">
-                                <label>Item Price:</label>
-                                <input type="text" className="form-control col-md-6" onChange={this.handleChange2} />
-                            </div>
-                        </div>
-                    </div>
-                    <br />
-                    <div className="form-group">
-                        <button className="btn btn-primary">Add Item</button>
-                    </div>
-                </form>
-            </div>
+            <AppComponent count={0} />
         )
     }
 }
--- resources/assets/js/ViewModel/ItemViewModel.js
+++ resources/assets/js/ViewModel/ItemViewModel.js
@@ -2,52 +2,71 @@ import React, {Component} from 'react';
 import {browserHistory} from 'react-router';
 import BaseViewModel from './BaseViewModel';
 import ItemModel from '../Model/ItemModel';
+import DefineMap from 'can-define/map/map';
 
 var $this;
-class ItemViewModel extends BaseViewModel {
-    constructor(props){
-        super(props);
-        this.state = {productName: '', productPrice: '', value: '', items: ''};        
-        this.itemmodel = new ItemModel();   
-        this.handleChange1 = this.handleChange1.bind(this);
-        this.handleChange2 = this.handleChange2.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        $this = this;
-    }
-    handleChange1(e){
-        this.setState({
-          productName: e.target.value
-        })
-    }
-    handleChange2(e){
-        this.setState({
-          productPrice: e.target.value
-        })
-    }
-    handleSubmit(e){
-        e.preventDefault();
-        const products = {
-          name: this.state.productName,
-          price: this.state.productPrice
-        }
-        this.itemmodel.save(products, function(response){
-          console.log(response);
-          browserHistory.push('/display-item');
-        });
-    }
-    getData($obj=null){
-        this.itemmodel.getAll(function(response){
-            $this.setState({ items: response.data });
-            if($obj!=null){
-                $obj.setState({ items: response.data });
-            }
-        });
-    }
-    deleteItemSuper(id, callback){
-        $this.itemmodel.remove(id, function(response){            
-            callback(response);            
-        });        
-    }
-}
+// class ItemViewModel extends DefineMap {
+//     constructor(props){
+//         super(props);
+//         // this.state = {productName: '', productPrice: '', value: '', items: ''};        
+//         // this.itemmodel = new ItemModel();   
+//         // this.handleChange1 = this.handleChange1.bind(this);
+//         // this.handleChange2 = this.handleChange2.bind(this);
+//         // this.handleSubmit = this.handleSubmit.bind(this);
+//         // $this = this;
+//         this.count = 1;
+//     }
+//     increment() {
+//         return this.count++;
+//     }
+//     handleChange1(e){
+//         alert("ei");
+//         this.setState({
+//           productName: e.target.value
+//         })
+//         console.log(this.state.productName)
+//     }
+//     handleChange2(e){
+//         this.setState({
+//           productPrice: e.target.value
+//         })
+//     }
+//     handleSubmit(e){
+//         e.preventDefault();
+//         const products = {
+//           name: this.state.productName,
+//           price: this.state.productPrice
+//         }
+//         console.log(products);
+//         this.itemmodel.save(products, function(response){
+//           console.log(response);
+//           browserHistory.push('/display-item');
+//         });
+//     }
+//     getData($obj=null){
+//         this.itemmodel.getAll(function(response){
+//             $this.setState({ items: response.data });
+//             if($obj!=null){
+//                 $obj.setState({ items: response.data });
+//             }
+//         });
+//     }
+//     deleteItemSuper(id, callback){
+//         $this.itemmodel.remove(id, function(response){            
+//             callback(response);            
+//         });        
+//     }
+// }
 
+const ItemViewModel = DefineMap.extend({
+    count: 'number',
+    increment: function() {
+      return this.count++;
+    },
+    handleSubmit: function(e){
+        alert("ei");
+    },
+});
+
+  
 export default ItemViewModel;
